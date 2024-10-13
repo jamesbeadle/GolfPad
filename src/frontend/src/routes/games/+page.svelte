2ab01d1755a3334df02bdc85e358a9098a92e3d4
thillyt <73003317+thillyt@users.noreply.github.com>
2024-10-13 23:24:26 +0100

    modified:   src/frontend/src/routes/+page.svelte 	modified:   src/frontend/src/routes/games/+page.svelte 	new file:   src/frontend/src/routes/games/mulligans/+page.svelte 	new file:   src/frontend/src/routes/games/mulligans/[gameId]/+page.svelte 	new file:   src/frontend/static/golfCourse.png 	modified:   package-lock.json 	modified:   package.json 	modified:   src/frontend/src/app.css 	modified:   src/frontend/src/lib/components/games/show-select-game-modal.svelte 	modified:   src/frontend/src/lib/icons/collapse-icon.svelte 	modified:   src/frontend/src/lib/icons/copy-icon.svelte 	modified:   src/frontend/src/lib/icons/expand-icon.svelte 	modified:   src/frontend/src/lib/icons/home-icon.svelte 	modified:   src/frontend/src/lib/icons/icp-icon.svelte 	modified:   src/frontend/src/lib/icons/logo-icon.svelte 	modified:   src/frontend/src/lib/icons/logout-icon.svelte 	modified:   src/frontend/src/lib/icons/menu-icon.svelte 	modified:   src/frontend/src/lib/icons/profile-icon.svelte 	modified:   src/frontend/src/lib/icons/star-icon.svelte 	modified:   src/frontend/src/lib/services/auth-services.ts 	modified:   src/frontend/src/lib/services/worker-auth.services.ts 	modified:   src/frontend/src/lib/stores/user-store.ts 	modified:   src/frontend/src/lib/workers/auth-worker.ts 	modified:   tailwind.config.js 	src/frontend/src/lib/stores/golfer-summaries-store.ts

--- src/frontend/src/routes/games/+page.svelte
+++ src/frontend/src/routes/games/+page.svelte
@@ -1,21 +1,218 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { writable } from "svelte/store";
     import Layout from "../Layout.svelte";
+    import { goto } from "$app/navigation";
+    import { golferGameSummary, getGolferGameSummary } from "$lib/stores/golfer-summaries-store";
+    import ShowSelectGameModal from "$lib/components/games/show-select-game-modal.svelte";
 
-    onMount(() => {
-        
+    let showNewGameModal = false;
+
+    let pageFilters = {
+        limit: BigInt(0),
+        offset: BigInt(0),
+    };
+
+    // Fetch golfer game summaries on mount
+    onMount(async () => {
+        try {
+            const result = await getGolferGameSummary(pageFilters);
+            console.log(result);  // Log the result after it's fetched
+        } catch (err) {
+            console.error("Failed to fetch golfer game summaries:", err);
+        }
     });
+    //TODO needs to be more like figma
+    function openGameModal() {
+        showNewGameModal = true;
+    }
+
+    function closeGameModal() {
+        showNewGameModal = false;
+    }
+
+    function handleGameSelection(event: CustomEvent) {
+        const gameChoice = event.detail;
+        closeGameModal();
+        goto(`/${gameChoice}-new`);
+    }
 
 </script>
 
 <Layout>
-    <div class="w-full max-w-4xl p-4 mx-auto text-black">
-        <h2 class="mt-3 mb-6 text-2xl font-black text-black md:text-4xl">
-            MY GAMES
-        </h2>
+    <div class="w-full">
+        <div class="w-full h-full p-2 px-4 text-black">
+            <div class="flex items-center justify-between mb-4">
+                <h2 class="mt-3 mb-4 text-3xl font-black text-black md:text-5xl condensed">MY GAMES</h2>
+                <button on:click={openGameModal} class="btn btn-new-game">New Game</button>
+                {#if showNewGameModal}
+                    <ShowSelectGameModal visible={showNewGameModal} closeModal={closeGameModal} on:gameSelected={handleGameSelection} />
+                {/if}
+            </div>
 
+            <!-- Headings -->
+            <div class="flex items-center p-4 font-bold table-headings condensed">
+                <div class="w-2/6">Game</div>
+                <div class="w-2/6">Players</div>
+                <div class="w-1/6">Status</div>
+                <div class="w-1/6"></div>
+            </div>
 
-        
+            <!-- Check if there are no games -->
+            {#if $golferGameSummary && $golferGameSummary.totalEntries === BigInt(0)}
+                <!-- Content to display when totalEntries is 0 -->
+                <p>No game history found. Start your first game!</p>
+            {/if}
+
+            <!-- Game List -->
+            {#if $golferGameSummary && $golferGameSummary.entries.length > 0}
+                {#each $golferGameSummary.entries as game}
+                    <div class="game-list">
+                        <div class="flex items-center p-4 game-type">
+                            <div class="flex items-center game-info">
+                                <!-- Game image -->
+                            </div>
+                            <div class="ml-4">
+                                <h3 class="font-bold">{game.gameType}</h3>
+                                <p class="text-sm">{new Date(Number(game.date) * 1000).toLocaleDateString()}</p>
+                            </div>
+                        </div>
+
+                        <!-- Players Column -->
+                        <div class="flex ml-auto players">
+                            {#each game.players as player}
+                                <div class="hover-picture">
+                                    <div class="p-2 bg-white rounded shadow-lg hover-player">
+                                        <p class="font-bold">{player}</p>
+                                        <button class="btn btn-view-player">View Player</button>
+                                    </div>
+                                </div>
+                            {/each}
+                        </div>
+
+                        <!-- Status Column -->
+                        <div class="w-1/6 font-bold text-blue-500 status">
+                            {game.status}
+                        </div>
+
+                        <!-- Button Column -->
+                        <div class="w-1/6 actions">
+                            <button class="btn btn-predict">Predict</button>
+                        </div>
+                    </div>
+                {/each}
+            {/if}
+        </div>
     </div>
 </Layout>
+
+
+<style>
+    /* Example button styling */
+    .btn {
+        padding: 10px 20px;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .btn-new-game {
+        background-color: #f6c200;
+        color: #1C4932;
+        border: none;
+    }
+
+    .btn-predict {
+        background-color: #007bff;
+        color: white;
+        padding: 10px 15px;
+    }
+    
+    .table-headings{
+        background-color: #f7f7f7;
+        padding: 10px;
+        font-size: 20px;
+        width:100%;
+        text-align: left;
+        font-weight: bold;
+    }
+
+    .game-type h3 {
+        font-weight: bold;
+    }
+
+    .game-list {
+        background-color: #f7f7f7;
+        border-top: 1px solid #eee;
+        margin-top: 20px;
+        text-align: left;
+        width: 100%;
+    }
+
+    .game-type {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #eee;
+        padding: 20px 0;
+    }
+
+    .game-info {
+        width: 60px;
+        height: 60px;
+        border-radius: 5px;
+    }
+
+    .players {
+        display: flex;
+        background-color: #f7f7f7;
+    }
+
+    .hover-picture {
+        display: none;
+        position: absolute;
+        top: 50px;
+        left: 0;
+        z-index: 100;
+    }
+
+    .hover-player {
+        background-color: white;
+        border-radius: 8px;
+        box-shadow: 0px 4px 8px rgba(0,0,0,0.1);
+        padding: 10px;
+    }
+
+    .btn-view-player {
+        background-color: #007bff;
+        color: white;
+        padding: 5px 10px;
+        border-radius: 5px;
+    }
+
+    .status {
+        font-size: 18px;
+        font-weight: bold;
+        color: #007bff;
+        background-color: #f7f7f7;
+    }
+
+    .flex {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+/*     .no-games-message {
+        text-align: center;
+        margin-top: 20px;
+        font-size: 18px;
+        font-weight: bold;
+        color: gray;
+    } */
+
+    h2 {
+        text-align: left;
+        margin-left: 0;
+    }
+
+</style>
---
+++ src/frontend/src/routes/games/mulligans/[gameId]/+page.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Layout from "../../../Layout.svelte";
+    export let params;    
+</script>
+
+<Layout>
+    <div class="w-full">
+        <div class="w-full p-2 px-4 text-black">
+            <div class="flex items-center justify-between">
+                <h2 class="mt-1 text-3xl font-black text-black md:text-5xl condensed">GAME DETAILS</h2>
+            </div>
+        </div>
+        <div class="flex w-full p-4 text-black bg-gray-100 ">
+            <div class="w-1/3">
+                <img src="/hero.png" alt="Mulligans" class="game-image" />
+            </div>
+
+            <div class="flex flex-col items-start justify-center w-1/3 ml-8"style="margin-top: 10px;" >
+                <span class="mb-0 text-sm font-medium text-gray-500 ">GAMETYPE</span>
+                <h1 class="font-black text-7xl game-type condensed"style="line-height: 1;">
+                    MULLI-<br>GANS
+                </h1>
+                <!--TODO needs to be replaced with date of game-->
+                <div class= "flex flex-col"style="margin-top: 10px;">
+                    <span class="mb-0 text-sm font-medium text-gray-500"style="margin-top: 10px;">DATE</span>
+                    <h3 class ="mb-4 text-4xl font-bold text-center condensed game-date">October 9 2024</h3>
+                </div>
+                <!--TODO needs to be replaced with actual course-->
+                <div class="flex items-center justify-start course-details"style="margin-top: 10px;" >
+                    <img src="/golfCourse.png" alt="Course" class="w-20 h-20 mr-4 rounded-lg course-image"/>
+                    <div class= "flex flex-col">
+                        <span class="mb-0 text-sm font-medium text-gray-500">COURSE</span>
+                        <div>
+                            <p class="text-3xl font-bold text-center condensed game-date">Course's Name</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <!--TODO needs to be replaced with game info-->
+            <div class="w-1/3">
+                <h4 class="mt-8 mb-0 text-3xl font-bold condensed"style="margin-top: -3px;">Player Details</h4>
+                <div class="mt-4 bg-white player-details">
+                    <div class="flex flex-col mb-3">
+                        <div class="flex px-4 mt-6 mb-2 space-x-2">
+                            <img src="/team/james.jpg" alt="Player 1" class="w-10 h-10 rounded-full">
+                            <img src="/team/josh.jpg" alt="Player 2" class="w-10 h-10 rounded-full">
+                            <img src="/team/george.jpg" alt="Player 3" class="w-10 h-10 rounded-full">
+                        </div>
+                        <hr class="my-2 mb-5 border-gray-300 border-t-1"style="border-color: #F3F3F3;"/>
+                        <div class ="flex items-center justify-between bg-white">
+                            <div class="flex flex-col px-4">
+                                <div class="font-medium text-gray-500 text-xxs">PLAYER</div>
+                                <p class="mb-2 text-lg font-bold condensed">James (18)</p>
+                            </div>
+
+                            <div class="flex flex-col">
+                                <div class="font-medium text-gray-500 text-xxs">GAME PLACE</div>
+                                <p class="mb-2 text-lg font-bold condensed">WINNER</p>
+                            </div>
+
+                            <div class="flex flex-col px-4">
+                                <div class="font-medium text-gray-500 text-xxs">PLAYER</div>
+                                <p class="mb-2 text-lg font-bold condensed">78 (+6)</p>
+                            </div>
+                        </div>
+                        <hr class="mt-2 border-gray-300 border-t-1" style="border-color: #F3F3F3;" />
+                    </div>
+                    <div class="mt-2"style="margin-top: -15px;">
+                        <table class="flex flex-col w-full border-collapse table-auto ">
+                            <thread>
+                                <tr style="border-color: #F3F3F3; margin-top: -10px; border-top-width: 1px;">
+                                    <th class="p-4 pr-10 text-lg font-bold text-center border-b condensed"style="border-color: #F3F3F3;">Hole</th>
+                                    <th class="p-6 pr-10 text-lg font-bold text-center border-b condensed"style="border-color: #F3F3F3;">Par</th>
+                                    <th class="p-6 pr-6 text-lg font-bold text-center border-b condensed"style="border-color: #F3F3F3;">Score</th>
+                                    <th class="p-6 text-lg font-bold text-center border-b condensed"style="border-color: #F3F3F3;">+/- Par</th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                                <!--TODO needs to be replaced with game info-->
+                                <tr>
+                                    <td class="p-4 pl-6 text-lg font-bold text-center border-b condensed">1</td>
+                                    <td class="p-4 pl-16 text-lg text-center border-b">4</td>
+                                    <td class="p-4 pl-16 text-lg text-center border-b">4</td>
+                                    <td class="p-4 pl-16 text-lg text-center border-b">E</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</Layout>
+<style>
+    .game-image {
+        width: 100%;
+        height: auto;
+        border-radius:8px;
+    }
+    .course-image {
+        width: 80px;
+        height: 80px;
+        object-fit: cover;
+        border-radius: 8px;
+    }
+</style>
