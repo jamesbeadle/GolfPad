4c8141e6b8950f74578155277c57d6a797e286d3
jamesbeadle <jamesbeadle1989@gmail.com>
2025-03-12 06:50:10 +0000

    Reorder type defs

--- src/frontend/src/routes/games/+page.svelte
+++ src/frontend/src/routes/games/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { goto } from "$app/navigation";
+    import { authStore } from "$lib/stores/auth-store";
+    import { gameStore } from "$lib/stores/game-store";
+    import { toasts } from "$lib/stores/toasts-store";
+    import type { GameSummaries, GetGameSummaries } from "../../../../declarations/backend/backend.did";
+    
+    import Layout from "../Layout.svelte";
     import GameSummaryRow from "$lib/components/game/game-summary-row.svelte";
     import ListViewPanel from "$lib/components/shared/list-view-panel.svelte";
     import PaginationRow from "$lib/components/shared/pagination-row.svelte";
-    import { onMount } from "svelte";
-    import type { GameSummaries, GetGameSummaries } from "../../../../declarations/backend/backend.did";
-    import Layout from "../Layout.svelte";
-    import { gameStore } from "$lib/stores/game-store";
-    import { toasts } from "$lib/stores/toasts-store";
-    import { authStore } from "$lib/stores/auth-store";
     import LocalSpinner from "$lib/components/shared/local-spinner.svelte";
 
     let isLoading = true;
@@ -60,7 +61,6 @@
             isLoading = false;
         }
     }
-
 
 </script>
 <Layout>
