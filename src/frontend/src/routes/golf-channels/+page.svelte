b1ee015383673414faece452ee4a32e13b2501c3
jamesbeadle <jamesbeadle1989@gmail.com>
2025-03-18 10:25:31 +0000

    Update coming soon pages

--- src/frontend/src/routes/golf-channels/+page.svelte
+++ src/frontend/src/routes/golf-channels/+page.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { goto } from "$app/navigation";
-    import { authStore } from "$lib/stores/auth-store";
-    import { golfChannelStore } from "$lib/stores/golf-channel-store";
-    import { toasts } from "$lib/stores/toasts-store";
-    import type { GolfChannels, GetGolfChannels } from "../../../../declarations/backend/backend.did";
     
     import Layout from "../Layout.svelte";
     import ListViewPanel from "$lib/components/shared/list-view-panel.svelte";
     import LocalSpinner from "$lib/components/shared/local-spinner.svelte";
+    import { toasts } from "$lib/stores/toasts-store";
 
     let isLoading = true;
-    let golfChannels: GolfChannels | null = null;
-    let page = 1n;
-    let totalPages = 1n; 
 
     onMount( async () => {
     });
 
     async function createNew(){
-        
+        toasts.addToast({type: 'info', message: 'Golf Channels coming soon.'})
     }
 
 </script>
@@ -27,7 +20,7 @@
     {#if isLoading}
         <LocalSpinner />
     {:else}
-        <ListViewPanel title="GOLF CHANNELS" buttonTitle="ADD GOLF CHANNEL" buttonCallback={createNew}>
+        <ListViewPanel title="GOLF CHANNELS" buttonTitle="CREATE GOLF CHANNEL" buttonCallback={createNew}>
            <p>Golf Channels Coming Soon.</p>
         </ListViewPanel>
     {/if}
