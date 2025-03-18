b4b976fb2c93feb94a7aa21ec86f09fb78cde6e6
jamesbeadle <jamesbeadle1989@gmail.com>
2025-03-18 10:21:00 +0000

    Update confirm modal

--- src/frontend/src/lib/components/shared/confirm-modal.svelte
+++ src/frontend/src/lib/components/shared/confirm-modal.svelte
@@ -3,3 +3,11 @@
     export let onConfirm: () => void;
     export let onCancel: () => void;
 </script>
+
+<div class="flex flex-col">
+    <p>{message}</p>
+    <div class="flex-row flex">
+        <button on:click={onCancel} class="brand-button-cancel w-1/2">Cancel</button>
+        <button on:click={onConfirm} class="brand-button w-1/2">Confirm</button>
+    </div>
+</div>
